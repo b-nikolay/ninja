319bd3295ba605ccdaf7e3785d035f4544372c54
unknown <barmin.nicolay@gmail.com>
2024-05-29 23:55:08 +0400

    123

--- js/script.js
+++ js/script.js
@@ -1,20 +1,60 @@
 function openTab(evt, cityName) {
-    // Declare all variables
     var i, tabcontent, tablinks;
   
-    // Get all elements with class="tabcontent" and hide them
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
       tabcontent[i].style.display = "none";
     }
   
-    // Get all elements with class="tablinks" and remove the class "active"
     tablinks = document.getElementsByClassName("tablinks");
     for (i = 0; i < tablinks.length; i++) {
       tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
   
-    // Show the current tab, and add an "active" class to the button that opened the tab
     document.getElementById(cityName).style.display = "block";
     evt.currentTarget.className += " active";
   } 
+
+
+  var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
+
+const hamb = document.querySelector("#hamb");
+
+const body = document.body;
+
+const menu = document.querySelector(".menu");
+
+hamb.addEventListener("click", hambHandler);
+
+function hambHandler(e) {
+  e.preventDefault();
+  hamb.classList.toggle("active");
+  menu.classList.toggle("active");
+  body.classList.toggle("noscroll");
+}
+
+
+const links = Array.from(menu.children);
+
+links.forEach((link) => {
+  link.addEventListener("click", closeOnClick);
+});
+
+function closeOnClick() {
+
+  hamb.classList.remove("active");
+  body.classList.remove("noscroll");
+}
